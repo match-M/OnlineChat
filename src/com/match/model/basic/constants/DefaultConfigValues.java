91413388d55a38ff2b29e5124360b129ab17b044
match-M <81463672+match-M@users.noreply.github.com>
2023-08-03 17:48:06 +0800

    Update DefaultConfigValues.java

--- src/com/match/model/basic/constants/DefaultConfigValues.java
+++ src/com/match/model/basic/constants/DefaultConfigValues.java
@@ -1,7 +1,7 @@
 package com.match.model.basic.constants;
 
 public class DefaultConfigValues {
-    public static final String IP = "www.zj10086.online";
+    public static final String IP = "192.168.88.161";
     public static final Integer PORT = 8601;
     public static final Integer VIEW_INTRANET_TEST_PROMPT = 1;
 
